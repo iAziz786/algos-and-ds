0565651ee844d71f0847ea8140da8463df776a98
Mohammd Aziz <mdaziz067@gmail.com>
2017-11-20 20:09:39 +0530

    Print type and value

--- hello.go
+++ hello.go
@@ -12,5 +12,7 @@ func main() {
 		z complex128 = cmplx.Sqrt(-5 + 12i)
 	)
 
-	fmt.Println(ToBe, MaxInt, z)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
