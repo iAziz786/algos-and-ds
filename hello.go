e4a915d409c79b2de0b03a1f17c1033cb971c567
Mohammd Aziz <mdaziz067@gmail.com>
2017-11-20 20:14:14 +0530

    Add zero values checking

--- hello.go
+++ hello.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math/cmplx"
 )
 
 func main() {
-	var (
-		ToBe bool = false
-		MaxInt uint64 = 1<<64 - 1
-		z complex128 = cmplx.Sqrt(-5 + 12i)
-	)
+	var i int
+	var f float64
+	var b bool
+	var s string
 
-	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
-	fmt.Printf("Type: %T Value: %v\n", z, z)
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
 }
