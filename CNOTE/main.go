d6f52a00e5b754178a093bb68a9f5491be7cd492
Mohammmad Aziz <mdaziz067@gmail.com>
2018-01-31 22:20:38 +0530

    Add new solution to the coding challange

---
+++ CNOTE/main.go
@@ -0,0 +1,32 @@
+// Link to the problem https://www.codechef.com/problems/CNOTE
+
+package main
+
+import "fmt"
+
+func main() {
+	var T, X, Y, K, N, p, c int
+	fmt.Scan(&T)
+	for T > 0 {
+		var flag int
+		fmt.Scan(&X)
+		fmt.Scan(&Y)
+		fmt.Scan(&K)
+		fmt.Scan(&N)
+		for i := 0; i < N; i++ {
+			fmt.Scan(&p)
+			fmt.Scan(&c)
+			if flag == 0 {
+				if p >= (X-Y) && c <= K {
+					flag = 1
+				}
+			}
+		}
+		if flag > 0 {
+			fmt.Println("LuckyChef")
+		} else {
+			fmt.Println("UnluckyChef")
+		}
+		T--
+	}
+}
