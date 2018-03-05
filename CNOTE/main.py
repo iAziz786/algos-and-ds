1ff50a38a0bece98e01af97cf978d54deadbfef3
Mohammmad Aziz <mdaziz067@gmail.com>
2018-03-05 11:04:19 +0530

    Add solution for https://www.codechef.com/problems/CNOTE

---
+++ CNOTE/main.py
@@ -0,0 +1,18 @@
+def main():
+    T = int(input())
+    while T:
+        T -= 1
+        X, Y, K, N = list(map(int, input().split()))
+        lucky = False
+        for _ in range(N):
+            P, C = list(map(int, input().split()))
+            if P >= (X - Y) and C <= K:
+                lucky = True
+        if lucky:
+            print("LuckyChef")
+        else:
+            print("UnluckyChef")
+
+
+if __name__ == "__main__":
+    main()
