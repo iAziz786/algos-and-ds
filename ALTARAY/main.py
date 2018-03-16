4ce507d4e79ae8c4863243d60f8edf9c655c24f5
Mohammad Aziz <mdaziz067@gmail.com>
2018-03-16 16:50:07 +0530

    Add solution for https://www.codechef.com/problems/ALTARAY

---
+++ ALTARAY/main.py
@@ -0,0 +1,18 @@
+# Link to the problem: https://www.codechef.com/problems/ALTARAY
+
+
+def main():
+    T = int(input())
+    while T:
+        T -= 1
+        N = int(input())
+        A = [int(i) for i in input().split()]
+        ans = [1]*N
+        for i in range(N - 1, 0, -1):
+            if A[i] * A[i - 1] < 0:
+                ans[i - 1] += ans[i]
+        print(' '.join(map(str, ans)))
+
+
+if __name__ == "__main__":
+    main()
