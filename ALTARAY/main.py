e21f8fb6f434a85adaf05aa3902afe189b00e6c1
Mohammmad Aziz <mdaziz067@gmail.com>
2018-03-20 07:39:25 +0530

    Change in ALTARAY

--- ALTARAY/main.py
+++ ALTARAY/main.py
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-# Link to the problem: https://www.codechef.com/problems/ALTARAY
-
-
-def main():
-    T = int(input())
-    while T:
-        T -= 1
-        N = int(input())
-        A = list(map(int, input().split()))
-        pc, mpc = 0, 0
-        s = -1
-        ss = -1
-        for i in range(N - 1):
-            if (A[i] > 0 and A[i + 1] < 0) or (A[i] < 0 and A[i + 1] > 0):
-                if pc == 0:
-                    s = i
-                pc += 1
-                if pc > mpc:
-                    mpc = pc
-                    ss = s
-            else:
-                pc = 0
-        ans = [1] * (N)
-        if mpc < 1:
-            print(' '.join(map(str, ans)))
-        else:
-            mpc += 1
-            for i in range(ss, (ss + mpc)):
-                ans[i] = mpc
-                mpc -= 1
-            print(' '.join(map(str, ans)))
-
-
-if __name__ == '__main__':
-    main()
-||||||| merged common ancestors
-=======
 # Link to the problem: https://www.codechef.com/problems/ALTARAY
 
 
@@ -45,7 +7,7 @@ def main():
         T -= 1
         N = int(input())
         A = [int(i) for i in input().split()]
-        ans = [1]*N
+        ans = [1] * N
         for i in range(N - 1, 0, -1):
             if A[i] * A[i - 1] < 0:
                 ans[i - 1] += ans[i]
@@ -54,4 +16,3 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> 4ce507d4e79ae8c4863243d60f8edf9c655c24f5
