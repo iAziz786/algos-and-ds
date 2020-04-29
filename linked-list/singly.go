7d15422bb56b427a0d44ec30b3286f2a6c2c8966
Mohammad Aziz <aziz@zoop.one>
2020-04-29 21:12:06 +0530

    Some simple linked list

--- linked-list/singly.go
+++ linked-list/singly.go
@@ -2,11 +2,13 @@ package singly
 
 import "fmt"
 
+// Node will be used to create new nodes in the list
 type Node struct {
 	value int
 	next  *Node
 }
 
+// Print traverse the linked list and print each element
 func (n *Node) Print() {
 	for ; n != nil; n = n.next {
 		fmt.Println(n.value)
--- linked-list/singly_test.go
+++ linked-list/singly_test.go
@@ -7,10 +7,39 @@ import (
 	"testing"
 )
 
+func getStdoutLogs(fn func()) string {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	fn()
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	return fmt.Sprintf("%s", out)
+}
+
 func TestSingly(t *testing.T) {
-	first := Node{}
-	second := Node{}
-	third := Node{}
+	first := Node{value: 11}
+	second := Node{value: 22}
+	third := Node{value: 33}
+
+	first.next = &second
+	second.next = &third
+	// Start printing
+
+	output := getStdoutLogs(first.Print)
+
+	if output != "11\n22\n33\n" {
+		t.Errorf("expected %s got %s", "11\n22\n33", output)
+	}
+}
+
+func TestSinglyLinkedList_HeadIntersion(t *testing.T) {
+	first := Node{value: 11}
+	second := Node{value: 22}
+	third := Node{value: 33}
 
 	third.value = 33
 	third.next = nil
@@ -21,19 +50,11 @@ func TestSingly(t *testing.T) {
 	first.value = 11
 	first.next = &second
 
-	// Start printing
+	newHead := Node{value: 55, next: &first}
 
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
+	output := getStdoutLogs(newHead.Print)
 
-	first.Print()
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	output := fmt.Sprintf("%s", out)
-	if output != "11\n22\n33\n" {
-		t.Errorf("expected %s got %s", "11\n22\n33", output)
+	if output != "55\n11\n22\n33\n" {
+		t.Fatal("failed to insert node at head")
 	}
 }
